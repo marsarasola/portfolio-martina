a7975e181ab8311a7e1b87e614d8b50233f1c17e
marsarasola <sarasolamartina21@gmail.com>
2024-11-26 20:28:10 -0300

    SECOND COMMIT

--- src/app/features/project/projects-page/projects-page.component.ts
+++ src/app/features/project/projects-page/projects-page.component.ts
@@ -13,7 +13,26 @@ import { FormsModule } from '@angular/forms';
 })
 export class ProjectsPageComponent implements OnInit {
 
-  projects: any[] = [];
+  projects: any[] = [
+    {
+      title: "APIS",
+      description: "Get data from the APIs",
+      image: "https://cdn.pixabay.com/photo/2024/05/06/14/23/ai-generated-8743405_640.png",
+      link: "https://marsarasola.github.io/MARVEL-APIS/"
+    },
+    {
+      title: "Phonebook",
+      description: "Save contacts",
+      image: "https://img.freepik.com/foto-gratis/mujer-negocios-usando-portatil-cafeteria_23-2148002168.jpg?ga=GA1.1.1936787504.1715102624&semt=ais_hybrid",
+      link: "https://marsarasola.github.io/agendaDeContactos/agendaFinal/"
+    },
+    {
+      title: "Cocos's Pizza",
+      description: "Company's website",
+      image: "https://proyecto9.ecolan.com/assets/img/nuestraHistoria.png",
+      link: "https://proyecto9.ecolan.com/index.html"
+    }
+  ];
 
   private apiUrl = 'http://localhost:4000/projects'; // URL de la API
 
--- src/app/features/timeline/timeline-page/timeline-page.component.ts
+++ src/app/features/timeline/timeline-page/timeline-page.component.ts
@@ -11,7 +11,28 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./timeline-page.component.css'],
 })
 export class TimelinePageComponent implements OnInit {
-  events: any[] = [];
+  events: any[] = [
+    {
+      title: "Universidad Nacional de Mar del Plata",
+      date: "2020-2022",
+      description: "Computer Engineering"
+    },
+    {
+      title: "Academia Egg",
+      date: "2023",
+      description: "Full-stack web developer"
+    },
+    {
+      title: "Mar del Plata Programa",
+      date: "2023",
+      description: "Full-stack web developer"
+    },
+    {
+      title: "Utest Academy",
+      date: "2024",
+      description: "QA, Tester"
+    }
+  ];
 
   private apiUrl = 'http://localhost:3000/events'; // URL de la API
 
